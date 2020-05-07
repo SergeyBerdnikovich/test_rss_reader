3f85e112d077cfa5e2ea78a6e6dcf188c90f8ce1
Sergey Berdnikovich <sberdnikovich@sphereinc.com>
2020-05-07 10:55:30 +0300

    Add json tags to RssItem

--- pkg/rssreader/rss_item.go
+++ pkg/rssreader/rss_item.go
@@ -4,10 +4,10 @@ import "time"
 
 // RssItem is an entity which describes rss feed item
 type RssItem struct {
-	Title       string
-	Source      string
-	SourceURL   string
-	Link        string
-	PublishDate time.Time
-	Description string
+	Title       string    `json:"title"`
+	Source      string    `json:"source"`
+	SourceURL   string    `json:"source_url"`
+	Link        string    `json:"link"`
+	PublishDate time.Time `json:"publish_date"`
+	Description string    `json:"description"`
 }
